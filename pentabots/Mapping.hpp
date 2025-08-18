cc39cee1a6bbb8e2906ba6bd7f103ea41dd5daf2
cdayaratne <chirana.dayaratne@gmail.com>
2025-08-18 16:40:02 +1000

    Updated Mapping.Hpp
    - hard coded start coords
    - function to update robot location

--- pentabots/Mapping.hpp
+++ pentabots/Mapping.hpp
@@ -4,14 +4,8 @@
 #include "Driving.hpp"
 #include "Turning.hpp"
 
-
 #define SIZE 9         
 #define LARGEVAL 255
-
-#define START_X 0
-#define START_Y 0   // Set Start Coordinates
-#define GOAL_X 4        
-#define GOAL_Y 4   // Set Goal Coordinates
 
 namespace mtrn3100 {
 
@@ -29,14 +23,54 @@ struct Maze {
 
 class Mapping {
 public:
+
+  short currentX;
+  short currentY;
+  short startX = 0;
+  short startY = 0;
+  short goalX  = 4;   // Center cell for 9x9 maze
+  short goalY  = 4;
+  
   Mapping(mtrn3100::Driving& drive, mtrn3100::Turning& turn)
     : driveController(drive), turnController(turn) {
     maze = new_Maze();
+
+    currentX = startX;
+    currentY = startY;
+
+    // Remove cells
+    // Bottom Left
+    remove_cell(0, 0);   
+    remove_cell(1, 0);   
+    remove_cell(0, 1);   
+
+    // Bottom Right
+    remove_cell(7, 0);   
+    remove_cell(8, 0);   
+    remove_cell(8, 1);  
+
+    // Top Left
+    remove_cell(0, 7);   
+    remove_cell(0, 8);   
+    remove_cell(1, 8);  
+
+    // Top Right
+    remove_cell(7, 8);   
+    remove_cell(8, 7);   
+    remove_cell(8, 8);  
+
+
+    // Initialize flood values: goal = 0, all others = LARGEVAL
+    for (short x = 0; x < SIZE; ++x) {
+      for (short y = 0; y < SIZE; ++y) {
+        maze->map[x][y]->floodval = LARGEVAL;
+      }
+    }
+    maze->map[goalX][goalY]->floodval = 0;
   }
 
   Maze* new_Maze() {
     Maze* m = new Maze;
-
     for (short x = 0; x < SIZE; ++x) {
       for (short y = 0; y < SIZE; ++y) {
         Node* n = new Node;
@@ -46,13 +80,13 @@ public:
         n->wallUp = n->wallDown = n->wallLeft = n->wallRight = false;
         n->up = n->down = n->left = n->right = nullptr;
 
-        n->floodval = abs(x - GOAL_X) + abs(y - GOAL_Y);
+        n->floodval = abs(x - goalX) + abs(y - goalY);
 
         m->map[x][y] = n;
       }
     }
 
-    // Link neighbors
+// Link neighbors
     for (short x = 0; x < SIZE; ++x) {
       for (short y = 0; y < SIZE; ++y) {
         if (y > 0) m->map[x][y]->down = m->map[x][y - 1];
@@ -61,10 +95,10 @@ public:
         if (x < SIZE - 1) m->map[x][y]->right = m->map[x + 1][y];
       }
     }
-
     return m;
   }
 
+// Looks at all accessible neighboring cells (no wall between). Finds the smallest floodval among them.
   short get_smallest_neighbor(Node* n) {
     short smallest = LARGEVAL;
     if (n->left && !n->wallLeft && n->left->floodval < smallest)
@@ -78,6 +112,7 @@ public:
     return smallest;
   }
 
+// If a sensor sees something within 50mm, marks that side as a wall.
   void update_walls(Node* n) {
     driveController.updateLidar();
     if (driveController.getFrontDist() < 50) n->wallUp = true;
@@ -85,6 +120,7 @@ public:
     if (driveController.getRightDist() < 50) n->wallRight = true;
   }
 
+// loop Floodfill
   void propagate_floodfill() {
     bool updated;
     do {
@@ -92,6 +128,7 @@ public:
       for (short x = 0; x < SIZE; ++x) {
         for (short y = 0; y < SIZE; ++y) {
           Node* n = maze->map[x][y];
+          if (x == goalX && y == goalY) continue; // goal always 0
           short minNeighbor = get_smallest_neighbor(n);
           if (n->floodval != minNeighbor + 1) {
             n->floodval = minNeighbor + 1;
@@ -105,11 +142,40 @@ public:
   char decide_next_move(Node* n) {
     short smallest = get_smallest_neighbor(n);
     if (n->up && !n->wallUp && n->up->floodval == smallest) return 'f';
-    if (n->left && !n->wallLeft && n->left->floodval == smallest) return 'l';
-    if (n->right && !n->wallRight && n->right->floodval == smallest) return 'r';
-    if (n->down && !n->wallDown && n->down->floodval == smallest) return 'rr'; // turn around
+    if (n->left && !n->wallLeft && n->left->floodval == smallest) return 'lf';
+    if (n->right && !n->wallRight && n->right->floodval == smallest) return 'rf';
+    if (n->down && !n->wallDown && n->down->floodval == smallest) return 'rrf'; // turn around
     return 'x';
   }
+
+  void update_position(char move) {
+    if (move == 'f') {        // forward
+    currentY += 1;          
+    } else if (move == 'lf') { // left
+    currentX -= 1;
+    } else if (move == 'rf') { // right
+    currentX += 1;
+    } else if (move == 'rrf') { // turn around/back
+    currentY -= 1;
+    }
+  }
+
+// Remove the cells that are not in the maze
+  void remove_cell(short x, short y) {
+    if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return; // safety check
+    Node* n = maze->map[x][y];
+
+    // Treat the cell as fully blocked
+    n->wallUp = true;
+    n->wallDown = true;
+    n->wallLeft = true;
+    n->wallRight = true;
+
+    n->floodval = LARGEVAL;
+}
+
+
+
 
   Maze* getMaze() { return maze; }
 
